d7fa1fba11017114308f943b175154e1c0949337
diegitpush <97diegolopez@gmail.com>
2025-10-20 16:49:31 +0200

    more changes

--- diego_cv/diego_cv/settings.py
+++ diego_cv/diego_cv/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%r^qsy4u-v81a)*0!*ddr5(t*mn@f6d1)yw*e41g)in%)9i+24'
+SECRET_KEY = 'static-site-secret-key-not-used'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+if DEBUG:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+else:
+    ALLOWED_HOSTS = ['https://diegitpush.github.io/cv_app']
 
 
 # Application definition
